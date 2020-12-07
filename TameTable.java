fd0dccfbdab9e9502cf879ed6c2d69696657d23e
shosuke-aker <75617762+shosuke-aker@users.noreply.github.com>
2020-12-07 21:50:28 +0900

    initial commit.
    練習です。

---
+++ TameTable.java
@@ -0,0 +1,20 @@
+package ループ処理のネスト;
+
+public class TameTable {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <= 9; j++) {
+				int value = i * j;
+				System.out.print(i + "×" + j + "=" + value);
+				System.out.print(" ");
+			}
+			System.out.println("");
+			
+		}
+
+	}
+
+
+
+	}
